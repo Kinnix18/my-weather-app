e868f43daad86061a8b3eba903ff3efc07da201d
Kinnix18 <yanchuk137@gmail.com>
2022-12-25 05:26:03 +0200

    started to work on js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-alert("Hello!!!");
+"use strict";
+
+let apiKey = "ff4e01btd323751oc4afa5a4c3e73777";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Kyiv&key=${apiKey}&units=metric`;
+console.log(apiUrl);
